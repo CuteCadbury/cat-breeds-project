4e12322e3e60e35bbc3aaf3189f69339ac263a52
Heeji Hyun <wlgmlgus@gmail.com>
2023-02-24 01:37:52 -0500

    in progress

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-fetch('https://api.thecatapi.com/v1/breeds')
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const selectBreed = document.querySelector('#breedInput')
+//     //const selectBreed = document.getElementById('breedInput')
+//     fetch('https://api.thecatapi.com/v1/breeds')
+//    .then(res => res.json())
+//    .then(data => {
+//     let result = ""
+//     data.forEach(breeds => {
+//         result += `<option>${breeds.name}</option>`
+//     })
+//     selectBreed.innerHTML = result;
+//    })
+//    .catch(err => console.log(err))
+// })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const selectBreed = document.querySelector('#selectBreed')
+    //const selectBreed = document.getElementById('selectBreed')
+    fetch('https://api.thecatapi.com/v1/breeds')
    .then(res => res.json())
-   .then(data => console.log(data))
+   .then(data => {
+    data.forEach(breeds => {
+        console.log(breeds.name)
+    })
+   })
+   .catch(err => console.log(err))
+})
 
 
 
+
+
+
+// //submit button when clicked
+// let submitBtn = document.getElementById("submit-btn")
+
+// // let searchInput = document.getElementById("breed-input")
+
+// submitBtn.addEventListener("click", () => {
+//     let catBreed = breedInput.value
+//     let catUrl = `https://api.thecatapi.com/v1/breeds`
+//     console.log(catUrl)
+// })
 
 //delete when 'Delete' is clicked
-// card.querySelector('#Delete').addEventListener('click', () => {
-//     card.innerHTML = ''
+// const deleteBtn = document.getElementById("delete-btn")
+
+// deleteBtn.querySelector('.delete-btn').addEventListener('click', () => {
+//     deleteBtn.remove()
 // })
