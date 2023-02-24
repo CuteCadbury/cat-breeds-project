539223401ece957fd893d8d56544ed7f35bfce21
Heeji Hyun <wlgmlgus@gmail.com>
2023-02-23 20:27:25 -0500

    in progress

---
+++ index.js
@@ -0,0 +1,11 @@
+fetch('https://api.thecatapi.com/v1/breeds')
+   .then(res => res.json())
+   .then(data => console.log(data))
+
+
+
+
+//delete when 'Delete' is clicked
+// card.querySelector('#Delete').addEventListener('click', () => {
+//     card.innerHTML = ''
+// })
