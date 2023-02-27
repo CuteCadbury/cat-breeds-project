bc5b20bfccd68235a3cf1f0ae0d20ddeff97d743
Heeji Hyun <wlgmlgus@gmail.com>
2023-02-27 15:38:39 -0500

    in progress

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 //Dropdown
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
     const selectBreed = document.querySelector('#selectBreed')
-    //const selectBreed = document.getElementById('selectBreed')
-
-
+    //or const selectBreed = document.getElementById('selectBreed')
 
     fetch('https://api.thecatapi.com/v1/breeds')
    .then(res => res.json())
@@ -15,10 +13,7 @@ document.addEventListener('DOMContentLoaded', (e) => {
     selectBreed.innerHTML = result
    })
    .catch(err => console.log(err))
-    })
-
-   
-
+})
 
 // let submitBtn = document.querySelector("#submit-btn")
 // let searchInput = document.getElementById("breed-input")
@@ -37,15 +32,64 @@ document.addEventListener('DOMContentLoaded', (e) => {
 //     console.log(data)
 // })
 
+let breeds =[]
 
+function getBreedName(){
+    fetch("https://api.thecatapi.com/v1/breeds")
+    .then(res => res.json())
+    .then(res => {
+        breeds = Object.keys(res.message)
+        addBreedNameToDom(breeds)
+    })
+}
+
+function addBreedNameToDom(breeds){
+    const div = document.getElementById("card card-shadow")
+    breeds.map(breed => {
+        const h3 = document.getElementById("cat-breed")
+        const li1 = document.getElementById("description")
+        const li2 = document.getElementById("temperament")
+        const li3 = document.getElementById("lifespan")
+        const li4 = document.getElementById("origin")
+        const button = document.getElementById("delete-btn")
+
+        h3.textContent = breed
+        li1.textContent = breed
+        li2.textContent = breed
+        li3.textContent = breed
+        li4.textContent = breed
+        button.textContent = breed
+
+        div.append(h3, li1, li2. li3, li4, button)
+    })
+}
 
 //delete when 'Delete' is clicked
+// 1.
 // const deleteBtn = document.getElementById("delete-btn")
 
-// deleteBtn.querySelector('.delete-btn').addEventListener('click', () => {
-//     deleteBtn.remove()
+// deleteBtn.getElementById('.delete-btn').addEventListener('click', () => {
+//     console.log(deleteBtn)
 // })
 
-document.querySelector("delete-btn").addEventListener("click", () => {
-    alert("Deleted!")
-})
+// 2.
+// document.querySelector("delete-btn").addEventListener("click", () => {
+//     alert("Deleted!")
+// })
+
+
+
+
+
+// <!------------------GitHub Search App Try----------------->
+// const form = document.getElementById("search-wrapper")
+
+// form.addEventListener("submit", () => {
+//     e.preventDefault()
+
+// })
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     console.log(document.querySelectorById('search-wrapper'))
+// })
+
